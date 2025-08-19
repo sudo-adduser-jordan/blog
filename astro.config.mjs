c6ada48dfe3691c13388c99b98536d9db6b9a250
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-18 20:45:46 -0700

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+    site: 'https://sudo-adduser-jordan.github.io',
+    base: 'test',
 	integrations: [mdx(), sitemap()],
 });
